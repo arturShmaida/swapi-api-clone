8038b63d35b972c3f23ea72056e360b9b4eefdcc
Artur Shmaida <artur.shmaida@gmail.com>
2024-07-13 10:44:00 +0200

    Changes in database config.

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,8 +6,9 @@ export default registerAs('database', () => ({
   port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  synchronize: true,
   database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
+  autoLoadEntities: true,
+  synchronize: false,
 }));
